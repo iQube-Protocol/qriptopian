62fdbc8fefc2865209888bc8474617297f979591
Kn0w1 <62913337+Kn0w-1@users.noreply.github.com>
2026-08-16 22:53:55 -0400

    Add Thresholds essay routes

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Layout } from "@/components/Layout";
 import Index from "./pages/Index";
+import Thresholds from "./pages/Thresholds";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
@@ -19,7 +20,8 @@ const App = () => (
           <Routes>
             <Route path="/" element={<Index />} />
             <Route path="/console" element={<Index />} />
-            {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+            <Route path="/thresholds" element={<Thresholds />} />
+            <Route path="/thresholds/:slug" element={<Thresholds />} />
             <Route path="*" element={<NotFound />} />
           </Routes>
         </Layout>
